588a9d3dda4bea26069da1cf13cd401275ea7829
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-09-04 19:12:41 +0000

    feat: error handling enhanced (now possible to handle api and)

--- src/common/shared/utils/handlers/error/global.ts
+++ src/common/shared/utils/handlers/error/global.ts
@@ -3,6 +3,7 @@
 import { Request, Response, NextFunction } from 'express';
 import ErrorResponse from './response';
 import ApiResponse from '../api-reponse';
+import { ViewService } from '../../../services';
 
 const errorHandler = (
   err: any,
@@ -10,6 +11,20 @@ const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
+  const viewService = new ViewService();
+
+  if (req.accepts('html')) {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'An unexpected error occurred';
+    return viewService.renderErrorPage(
+      req,
+      res,
+      statusCode,
+      message,
+      'Error Occurred',
+    );
+  }
+
   if (err instanceof ErrorResponse) {
     return ApiResponse.error(res, { success: false, error: err });
   }
