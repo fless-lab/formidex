f68e18570ecdb761d9b6cffd0ca24074dcc56741
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-09-11 18:08:03 +0000

    update: enhanced session init in order to enclude nessary thing for better handling client web requests

--- src/core/framework/session-flash/index.ts
+++ src/core/framework/session-flash/index.ts
@@ -1,15 +1,27 @@
 import { Application } from 'express';
 import session from 'express-session';
 import flash from 'connect-flash';
+import RedisStore from 'connect-redis';
 import { config } from '../../config';
+import { DB } from '../../../core/framework';
+
+const redis = DB.redis;
+redis.init();
+const client = redis.getClient();
 
 export const initializeSessionAndFlash = (app: Application): void => {
   app.use(
     session({
+      store: new RedisStore({ client }),
       secret: config.session.secret,
       resave: false,
-      saveUninitialized: true,
-      cookie: { secure: config.runningProd },
+      saveUninitialized: false,
+      cookie: {
+        secure: config.runningProd,
+        maxAge: config.session.maxAge,
+        sameSite: config.session.sameSite,
+        httpOnly: config.session.httpOnly,
+      },
     }),
   );
   app.use(flash());
