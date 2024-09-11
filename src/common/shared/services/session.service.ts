02a57afd80ba43d2be0b7dd12f769ef4ece6ce3d
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-09-11 18:13:03 +0000

    feat: added session service

---
+++ src/common/shared/services/session.service.ts
@@ -0,0 +1,44 @@
+import { Request } from 'express';
+import { IUser } from '../../../apps';
+import { logger } from './logger.service';
+
+export class SessionService {
+  static storeUserInSession(req: Request, user: IUser): void {
+    req.session.user = user;
+  }
+
+  static getUserIdFromSession(req: Request): string | null {
+    return req.session.user?.id || null;
+  }
+
+  static clearSession(req: Request): void {
+    req.session.destroy((err) => {
+      if (err) {
+        logger.error('Error while destroying user session', err);
+      }
+    });
+  }
+
+  static storeTokensInSession(
+    req: Request,
+    accessToken: string,
+    refreshToken: string,
+  ): void {
+    req.session.tokens = {
+      access: accessToken,
+      refresh: refreshToken,
+    };
+  }
+
+  static getAccessTokenFromSession(req: Request): string | null {
+    return req.session.tokens?.access || null;
+  }
+
+  static getRefreshTokenFromSession(req: Request): string | null {
+    return req.session.tokens?.refresh || null;
+  }
+
+  static isAuthenticated(req: Request): boolean {
+    return !!req.session.user;
+  }
+}
