e07f1a5cb65ab980d8776966e864df0e78900300
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-09-13 14:00:27 +0000

    feat: auth.views routes and controllers added

---
+++ src/apps/auth/routes/auth.views.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { AuthViewsController } from '../controllers';
+
+const router = Router();
+const authViewsController = new AuthViewsController();
+
+router.get(
+  '/login',
+  authViewsController.showLoginPage.bind(authViewsController),
+);
+router.get(
+  '/register',
+  authViewsController.showRegisterPage.bind(authViewsController),
+);
+router.get(
+  '/verify',
+  authViewsController.showVerifyPage.bind(authViewsController),
+);
+
+router.get(
+  '/login/otp',
+  authViewsController.showGenerateOtpPage.bind(authViewsController),
+);
+router.get(
+  '/login/otp/confirm',
+  authViewsController.showConfirmOtpPage.bind(authViewsController),
+);
+
+router.get(
+  '/forgot-password',
+  authViewsController.showForgotPasswordPage.bind(authViewsController),
+);
+router.get(
+  '/reset-password',
+  authViewsController.showResetPasswordPage.bind(authViewsController),
+);
+
+export default router;
--- src/apps/auth/routes/index.ts
+++ src/apps/auth/routes/index.ts
@@ -1,2 +1,3 @@
-export { default as AuthRoutes } from './auth.routes';
+export { default as AuthViewsRoutes } from './auth.views.routes';
+export { default as AuthRoutes } from './_auth.logic.routes';
 export { default as OTPRoutes } from './otp.routes';
