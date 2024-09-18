132dda618339ddb4ff7311591b8d6337e32536cc
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-09-18 09:10:28 +0000

    refactor: user routes deletet

--- src/common/global-router/index.ts
+++ src/common/global-router/index.ts
@@ -4,14 +4,12 @@ import {
   MicroSaasRoutes,
   OTPRoutes,
   PublicAccessRoutes,
-  UserRoutes,
 } from '../../apps';
 
 const router = Router();
 
 router.use('/', PublicAccessRoutes);
 router.use('/micro-saas', MicroSaasRoutes);
-router.use('/users', UserRoutes);
 router.use('/otp', OTPRoutes);
 router.use('/auth', AuthRoutes);
 
