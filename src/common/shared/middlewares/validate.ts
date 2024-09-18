366c9bbede8b5a38c880d2e9007ca8d97fb28fbf
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-09-18 09:18:22 +0000

    perf: validate middleware updated in order to handle redirect use cases

--- src/common/shared/middlewares/validate.ts
+++ src/common/shared/middlewares/validate.ts
@@ -1,22 +1,38 @@
 import { Request, Response, NextFunction } from 'express';
 import { ObjectSchema } from 'joi';
 import { ApiResponse, ErrorResponse } from '../utils';
+import { ViewService } from '../services';
 
-export const validate = (schema: ObjectSchema) => {
+const viewService = new ViewService();
+
+export const validate = (schema: ObjectSchema, viewName?: string) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
     if (error) {
       const { details } = error;
-      const message = details.map((i) => i.message).join(',');
-      const errorResponse = new ErrorResponse('VALIDATION_ERROR', message);
-      return ApiResponse.error(res, {
-        success: false,
-        error: {
-          message: errorResponse.message,
-          suggestions: errorResponse.suggestions,
-          statusCode: errorResponse.statusCode,
-        } as any,
-      });
+      const errorMessages = details.map((i) => i.message);
+      const errorResponse = new ErrorResponse(
+        'VALIDATION_ERROR',
+        errorMessages.join(','),
+      );
+      if (req.accepts('html')) {
+        const generalErrorView = 'errors/500';
+        viewService.handleError(
+          req,
+          res,
+          errorResponse,
+          viewName || generalErrorView,
+        );
+      } else {
+        return ApiResponse.error(res, {
+          success: false,
+          error: {
+            message: errorResponse.message,
+            suggestions: errorResponse.suggestions,
+            statusCode: errorResponse.statusCode,
+          } as any,
+        });
+      }
     } else {
       next();
     }
